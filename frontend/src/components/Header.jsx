09ee42bcdba6cae8c5b6e3ae485c68657def5042
G1lber <martinezfrancogilber@gmail.com>
2025-06-13 07:43:14 -0500

    Cambio logo de terminal

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,22 +4,25 @@ const Header = () => {
   const location = useLocation();
 
   const links = [
-    { path: "/perfil", label: "Perfil" },
-    { path: "/login", label: "Logout" },
+    { path: "/", label: "Logout" },
   ];
 
   return (
     <header className="bg-[#222831] text-[#EEEEEE] px-6 py-4 shadow-md">
-      <nav className="flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-[#00ADB5]">
-          Terminal
+      <nav className="flex justify-between items-center ">
+        {/* Logo + Title */}
+        <Link to="/" className="flex items-center gap-3">
+          <img src="../../lgterminal.png" alt="logo" className="w-12 h-12" />
+          <span className="text-2xl font-bold text-[#00ADB5]">Terminal</span>
         </Link>
-        <div className="space-x-6 text-sm">
+
+        {/* Navigation Links */}
+        <div className="flex space-x-6 text-sm">
           {links.map(({ path, label }) => (
             <Link
               key={path}
               to={path}
-              className={`transition-colors hover:text-[#00ADB5] ${
+              className={`transition-colors duration-200 hover:text-[#00ADB5] ${
                 location.pathname === path ? "text-[#00ADB5] font-semibold" : ""
               }`}
             >
